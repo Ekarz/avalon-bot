6a0ff64ee41547e7594bba2d8a287eb779e5ee77
Antoine Robillard <antoine.robillard@noos.fr>
2020-01-22 20:15:06 +0100

    Setup basic features with help command

---
+++ src/commands/help.js
@@ -0,0 +1,32 @@
+const { prefix } = require('../config.json');
+
+exports.name = 'help';
+exports.description = 'List all commands and their description.';
+exports.usage = '[command name]';
+exports.execute = (message, args) => {
+    const data = [];
+    const { commands } = message.client;
+
+    if (!args.length) {
+        data.push('Available commands :');
+        data.push(commands.map(command => command.name).join(', '));
+        data.push(`For more info, use \`${prefix}help [command name]\`.`);
+
+        return message.author.send(data, { split: true });
+    }
+
+    const name = args[0].toLowerCase();
+    const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+    if (!command) {
+        return message.reply('This command does not exist.');
+    }
+
+    data.push(`**Name:** ${command.name}`);
+
+    if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+    if (command.description) data.push(`**Description:** ${command.description}`);
+    if (command.usage) data.push(`**Usage:** \`${prefix}${command.name} ${command.usage}\``);
+
+    message.author.send(data, { split: true });
+};
---
+++ src/index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const { token, prefix } = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('src/commands')
+                       .filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if (!client.commands.has(commandName)) return;
+    const command = client.commands.get(commandName);
+
+    try {
+        command.execute(message, args);
+    } catch (e) {
+        console.error(e);
+    }
+});
+
+client.login(token);
