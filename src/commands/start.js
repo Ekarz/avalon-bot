cddbe7ff97314e1c2b8de96747f9592bc0ee79e6
Antoine Robillard <antoine.robillard@teemo.co>
2020-01-23 14:00:42 +0100

    Add start command
    No tests yet

---
+++ src/commands/start.js
@@ -0,0 +1,13 @@
+const state = require('../game/state');
+
+exports.name = 'start';
+exports.channelOnly = true;
+exports.description = 'Start the game';
+exports.usage = '';
+exports.execute = (message, args) => {
+    if (state.playerTags.length < 5 || message.author !== state.host) {
+        return message.react('🚫');
+    }
+
+    message.react('👍');
+};
